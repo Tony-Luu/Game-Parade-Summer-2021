51b276fac47d10c311c3950c835070532d724e5b
Aayush Mathur <aayushmathur14@gmail.com>
2021-07-18 16:31:33 +0100

    Movable boxes can be picked up and dropped

--- Game Parade Summer 2021/Assets/AM_Scripts/AM_KillPlayer.cs
+++ Game Parade Summer 2021/Assets/AM_Scripts/AM_KillPlayer.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class AM_KillPlayer : MonoBehaviour
 {
     [SerializeField] private Transform checkpoint;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             print("die");
-            other.gameObject.transform.position = checkpoint.transform.position;
+            other.gameObject.GetComponent<playermovementscript>().enabled = false;
+            other.gameObject.transform.position = new Vector3(checkpoint.transform.position.x, checkpoint.transform.position.y, 
+                checkpoint.transform.position.z);
+            StartCoroutine(WaitBeforeMoving());
         }
+    }
+
+    IEnumerator WaitBeforeMoving()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<playermovementscript>().enabled = true;
     }
 }
--- Game Parade Summer 2021/Assets/AM_Scripts/AM_PickupObject.cs
+++ Game Parade Summer 2021/Assets/AM_Scripts/AM_PickupObject.cs
@@ -6,9 +6,19 @@ public class AM_PickupObject : MonoBehaviour
 {
     public bool isShovelInHand = false;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && GameObject.FindGameObjectWithTag("Box").transform.parent != null)
+        {
+            GameObject.FindGameObjectWithTag("Box").transform.parent = null;
+            GameObject.FindGameObjectWithTag("Box").GetComponent<Rigidbody>().isKinematic = false;
+            GameObject.FindGameObjectWithTag("Box").GetComponent<BoxCollider>().isTrigger = false;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && GameObject.FindGameObjectWithTag("ObjectHolder").transform.childCount == 0)
         {
             transform.SetParent(GameObject.FindGameObjectWithTag("ObjectHolder").transform);
             transform.position = GameObject.FindGameObjectWithTag("ObjectHolder").transform.position;
